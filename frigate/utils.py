7286d8e85ad5ba92f1701e2f61f6905a6ae7ac4a
Jacob Tomlinson <jtomlinson@nvidia.com>
2019-11-28 13:39:44 +0000

    Add docstring

--- frigate/utils.py
+++ frigate/utils.py
@@ -2,6 +2,24 @@ import collections.abc
 
 
 def flatten(l):
+    """Flatten a list of nested lists with unknown depth.
+
+    Iterate over nested lists and yield each item sequentially regardless
+    of the nesting.
+
+    Examples:
+        Traversing the following YAML config would yield this list.
+
+        >>> list(flatten([['a', 'b'], ['c', 'd'], ['e', 'f', ['g', 'h']]])
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+    Args:
+        l (list): Nested list of lists of lists of lists, etc.
+
+    Yields:
+        obj: Flattened list items.
+
+    """
     for el in l:
         if isinstance(el, collections.abc.Iterable) and not isinstance(
             el, (str, bytes)
